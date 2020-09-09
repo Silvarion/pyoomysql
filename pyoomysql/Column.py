0c4e6c39c53e8d42bfc555ea34fb1e23404830a5
Jesus Alejandro Sanchez Davila <jsanchez.consultant@gmail.com>
2020-09-09 20:48:00 +0200

    Column preview update

--- pyoomysql/Column.py
+++ pyoomysql/Column.py
@@ -2,8 +2,17 @@ from .Table import Table
 
 class Column:
     # Constructor
-    def __init__(self, table: Table, name: str, length: int, precision: int, nullable: bool, autoincrement: bool, charset: str = self.table.charset):
-        pass
+    def __init__(self, table: Table, name: str, length: int, precision: int, nullable: bool = True, autoincrement: bool = False, charset: str = None):
+        self.table = table
+        self.name = name
+        self.length = length
+        self.precision = precision
+        self.nullable = nullable
+        self.autoincrement = autoincrement
+        if charset is None:
+            charset = self.table.schema.charset
+        else:
+            self.charset = charset
 
     # Python Object Overrides
     def __str__(self):
