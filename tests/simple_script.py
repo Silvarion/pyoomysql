6a643f227250f63dd4aa744830e33c02ade93e6d
Jesus Alejandro Sanchez Davila <jsanchez.consultant@gmail.com>
2020-09-09 21:19:48 +0200

    Cleanup and gitignore

--- tests/simple_script.py
+++ tests/simple_script.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
+
 import json
 import logging
 import pyoomysql
+from argparse import ArgumentParser
 
-mydb = pyoomysql.Database(hostname="hekkamiahmsv1c.mylabserver.com", port=3306)
-mydb.connect(user="mysqldba", password="mysqlpass", log_level=logging.INFO)
-user_list = mydb.get_users()
+parser = ArgumentParser()
+parser.add_argument("-h","--host", dest="db_host", required=True)
+parser.add_argument("-P","--port", dest="db_port", required=True)
+parser.add_argument("-u","--user", dest="db_user", required=True)
+parser.add_argument("-p","--password", dest="db_pswd", required=True)
 
+args = parser.parse_args()
+
+mydb = pyoomysql.Database(hostname=args.db_host, port=args.db_port)
+mydb.connect(user=args.db_user, password=args.db_pswd, log_level=logging.INFO)
+for user in mydb.get_users():
+    print(user)
 
 mytestschema = pyoomysql.Schema(database=mydb, name="my_test_schema")
 mytestschema.create()
@@ -24,4 +35,3 @@ users_table = pyoomysql.Table(schema=mysql_schema, name="users")
 my_test_user = pyoomysql.User(database=mydb, user="my_test_user", password="my_test_password", host="anywhere_in_the_world")
 my_test_user.create()
 my_test_user.change_attr("host","my_new_host")
-#my_test_user.update()
