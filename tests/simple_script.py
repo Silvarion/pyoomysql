c291f0fb1fa7ca367186888f4e176977400d2d30
Jesus Sanchez <jsanchez.consultant@gmail.com>
2020-09-09 19:43:56 +0200

    User update completed (#5)
    * Update attributes
    
    * Completed user update and changed username for user to keep consistency

--- tests/simple_script.py
+++ tests/simple_script.py
@@ -4,18 +4,26 @@ import pyoomysql
 mydb = pyoomysql.Database(hostname="hekkamiahmsv1c.mylabserver.com", port=3306)
 mydb.connect(username="mysqldba", password="mysqlpass", log_level=logging.DEBUG)
 
+mydb = pyoomysql.Database(hostname="hekkamiahmsv1c.mylabserver.com", port=3306)
+mydb.connect(user="mysqldba", password="mysqlpass", log_level=logging.INFO)
 user_list = mydb.get_users()
+
 
 mytestschema = pyoomysql.Schema(database=mydb, name="my_test_schema")
 mytestschema.create()
 mytestschema.exists
 mytesttable = pyoomysql.Table(schema=mytestschema, name="my_test_table")
-script = """-- Set schema
-use my_test_schema;
--- 
-show tables;"""
-mydb.run(script=script)
+# script = """-- Set schema
+# use my_test_schema;
+# -- 
+# show tables;"""
+# mydb.run(script=script)
 mytestschema.drop()
 mytestschema.exists
 mysql_schema = pyoomysql.Schema(database=mydb, name="mysql")
 users_table = pyoomysql.Table(schema=mysql_schema, name="users")
+
+my_test_user = pyoomysql.User(database=mydb, user="my_test_user", password="my_test_password", host="anywhere_in_the_world")
+my_test_user.create()
+my_test_user.change_attr("host","my_new_host")
+#my_test_user.update()
