1d08f94142aa5310dd0d0edb75fd9765fddfee29
Jesus Alejandro Sanchez Davila <jsanchez.consultant@gmail.com>
2020-09-18 11:21:24 +0200

    Pumped version to 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyoomysql", # Replace with your own username
-    version="0.1.4",
+    version="0.1.5",
     author="Jesus Alejandro Sanchez Davila",
     author_email="jsanchez.consultant@gmail.com",
     description="Python Object-Orinted MySQL interface",
