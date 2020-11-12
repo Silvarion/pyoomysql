9750915c95da688c62000cc7310712479b0cfaaa
Jesus Alejandro Sanchez Davila <jsanchez.consultant@gmail.com>
2020-11-12 13:31:45 +0100

    Pumped up the setup version for the release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyoomysql", # Replace with your own username
-    version="0.1.12",
+    version="0.1.13",
     author="Jesus Alejandro Sanchez Davila",
     author_email="jsanchez.consultant@gmail.com",
     description="Python Object-Orinted MySQL interface",
