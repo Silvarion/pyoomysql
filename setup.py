419441e32b45af75f81d4269cb1abbf78769face
Jesus Sanchez <jsanchez.consultant@gmail.com>
2022-10-25 17:32:49 +0200

    Update setup.py
    Pump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyoomysql",
-    version="0.2.2",
+    version="0.2.3",
     author="Jesus Alejandro Sanchez Davila",
     author_email="jsanchez.consultant@gmail.com",
     description="Python Object-Orinted MySQL interface",
